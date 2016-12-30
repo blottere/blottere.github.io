0bd7a7555b8a3f2bbdcb1be38f588b9b0827a653
Nikhil Venkatesh <nikhilv.13@gmail.com>
2016-12-29 16:07:18 -0800

    Gulp Workflow update
    - Recreated project pages, other pages in progress
    - Restructured workspace and code to have less dependencies.

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+/** ----------------
+Nikhil Venkatesh
+26 Dec 2016
+---------------- **/
+
+// Packages -----------------------------------------
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+
+    // Styles
+    sass = require('gulp-sass'),
+    glob = require('gulp-sass-glob'),
+    cssNano = require('gulp-cssnano'),
+    combineMQ = require('gulp-combine-mq'),
+    prefix = require('gulp-autoprefixer'),
+
+    // Content
+    markdownIt = require('marked'),
+    prettify = require('gulp-prettify'),
+    include = require('gulp-file-include'),
+
+    // Scripts
+    uglify = require('gulp-uglify'),
+    newer = require('gulp-newer'),
+
+    // Utils
+    plumber = require('gulp-plumber'),
+    stripComments = require('gulp-strip-comments');
+
+// BrowserSync Static Server ------------------
+
+gulp.task('serve', ['css'], function() {
+
+    browserSync.init({
+        server: "./",
+        browser: "google chrome"
+    });
+
+    gulp.watch("styles/scss/**/*.scss", ['css']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+// Content ------------------------------------
+gulp.task('html', function() {
+    gulp.src('templates/pages/*.html')
+        .pipe(plumber())
+        .pipe(include({
+            basepath: '',
+            filters: {
+                markdown: markdownIt.setOptions()
+            }
+        }))
+        .pipe(prettify({
+            indent_with_tabs: "true"
+        }))
+        .pipe(gulp.dest('./'))
+});
+
+// Styles -------------------------------------
+gulp.task('css', function() {
+    return gulp.src('styles/scss/*.{scss,css}')
+        .pipe(plumber())
+        .pipe(sass()) // SASS Preprocessor
+        .pipe(combineMQ()) // Combine all Media Queries
+        .pipe(prefix({
+            browsers: ['last 4 versions']
+        }))
+        .pipe(cssNano({
+            reduceIdents: false
+        }))
+        .pipe(gulp.dest('styles'))
+        .pipe(browserSync.stream());
+});
+
+// Scripts ------------------------------------
+gulp.task('js', function() {
+    gulp.src('scripts/js/**/*.js')
+        .pipe(newer('scripts'))
+        .pipe(uglify())
+        .pipe(gulp.dest('scripts'));
+});
+
+// FUNCTIONS //--------------------------------
+// Watch Function
+gulp.task('watch', function() {
+    gulp.watch('templates/**/*.html', ['html']);
+    gulp.watch('styles/**/*.scss', ['css']);
+});
+
+// Default Function
+gulp.task('default', ['serve', 'watch'], function() {});
